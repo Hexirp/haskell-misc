3715372cc4362d0303f8a504ea0b9c82f2a83981
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2025-10-05 01:49:21 +0900

    Add: List モジュールの実装を追加する
    複雑な部分に時間がかかりそうなので、その前に簡単に実装できた部分を
    追加する。

--- spanning-tree/app/List.hs
+++ spanning-tree/app/List.hs
@@ -1,16 +1,26 @@
 module List (processEdgeList) where
 
+import Data.Map as Map (Map, insertWith, empty, singleton)
+import Data.Sequence as Seq (Seq (Empty, (:<|)), singleton)
+import Data.Set as Set (Set, singleton)
+
 processEdgeList :: String -> [(String, String)] -> [(String, String)]
-processEdgeList rootNode edgeList = undefined
+processEdgeList rootNode edgeList =
+  concatMap extractShortestPathEdgeList $ bfsShortestPathEdgeListMap rootNode $ buildAdjacencyMap edgeList
 
-foo :: String -> [(String, String)] -> [(String, String)]
-foo rootNode edgeList = filter (bar rootNode) edgeList
+buildAdjacencyMap :: [(String, String)] -> Map.Map String [String]
+buildAdjacencyMap = foldr (\(start_node, end_node) -> Map.insertWith (++) start_node [end_node]) Map.empty
 
-bar :: String -> (String, String) -> Bool
-bar rootNode edge = rootNode == startNode edge
+bfsShortestPathEdgeListMap :: String -> Map String [String] -> Map.Map String [String]
+bfsShortestPathEdgeListMap rootNode graphMap =
+  bfsShortestPathEdgeListMap_ graphMap (Set.singleton rootNode) (Seq.singleton rootNode) (Map.singleton rootNode [])
 
-startNode :: (String, String) -> String
-startNode (start_node, _) = start_node
+bfsShortestPathEdgeListMap_ :: Map.Map String [String] -> Set.Set String -> Seq.Seq String -> Map.Map String [String] -> Map.Map String [String]
+bfsShortestPathEdgeListMap_ graph_map visited_node_set focused_node_queue shortest_path_edge_list_map =
+  case focused_node_queue of
+    Empty -> shortest_path_edge_list_map
+    current_focused_node :<| rest_focused_node_queue -> undefined
 
-endNode :: (String, String) -> String
-endNode (_, end_node) = endNode
+extractShortestPathEdgeList :: [String] -> [(String, String)] 
+extractShortestPathEdgeList shortestPathEdgeList =
+  zip (reverse shortestPathEdgeList) (drop 1 $ reverse shortestPathEdgeList)
