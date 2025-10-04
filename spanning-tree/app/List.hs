a175b62de8e62db00f49f56ec0f39c43bf626715
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2025-10-05 01:12:31 +0900

    Add: List モジュールに試作の関数を追加する
    いまいち実装の方法が掴めないので、考察の助けにするために、
    必要そうな補助関数を追加してみる。

--- spanning-tree/app/List.hs
+++ spanning-tree/app/List.hs
@@ -1,4 +1,16 @@
 module List (processEdgeList) where
 
 processEdgeList :: String -> [(String, String)] -> [(String, String)]
-processEdgeList edgeList rootNodeName = undefined
+processEdgeList rootNode edgeList = undefined
+
+foo :: String -> [(String, String)] -> [(String, String)]
+foo rootNode edgeList = filter (bar rootNode) edgeList
+
+bar :: String -> (String, String) -> Bool
+bar rootNode edge = rootNode == startNode edge
+
+startNode :: (String, String) -> String
+startNode (start_node, _) = start_node
+
+endNode :: (String, String) -> String
+endNode (_, end_node) = endNode
