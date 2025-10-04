c64d40443139f7817f42b499c996987a3719fc27
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2025-10-05 00:45:16 +0900

    Add: graphviz パッケージに関する関数を実装する

--- spanning-tree/app/Graph.hs
+++ spanning-tree/app/Graph.hs
@@ -1,13 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Graph (processDotText) where
 
-import Data.Text.Lazy as LazyText (Text, unpack)
+import Data.Maybe (mapMaybe)
 
-import Data.GraphViz as GraphViz
-import Data.GraphViz.Printing as GraphViz (DotCode, renderDot, toDot)
+import Data.Foldable (Foldable (toList), forM_)
+
+import Data.Text.Lazy as LazyText (Text)
+
+import Data.GraphViz (parseDotGraph, GraphID (Str))
+import Data.GraphViz.Printing (PrintDot (toDot), renderDot)
+import Data.GraphViz.Types.Generalised as GraphViz (DotStatement (DE), DotGraph (graphStatements), DotEdge (DotEdge))
+import Data.GraphViz.Types.Monadic (digraph, edge)
 
 processDotText :: String -> LazyText.Text -> LazyText.Text
 processDotText rootNodeName dotText =
-  renderDotGraph $ processDotGraph rootNodeName $ GraphViz.parseDotGraph dotText
+  renderDotGraph $ processDotGraph rootNodeName $ parseDotGraph dotText
 
 processDotGraph :: String -> GraphViz.DotGraph String -> GraphViz.DotGraph String
 processDotGraph rootNodeName dotGraph =
@@ -17,10 +25,15 @@ processEdgeList :: String -> [(String, String)] -> [(String, String)]
 processEdgeList edgeList rootNodeName = undefined
 
 extractEdgeList :: GraphViz.DotGraph String -> [(String, String)]
-extractEdgeList = undefined
+extractEdgeList dotGraph = mapMaybe extractEdge $ toList $ GraphViz.graphStatements dotGraph
+
+extractEdge :: DotStatement String -> Maybe (String, String)
+extractEdge (DE (DotEdge start_point end_point _)) = Just (start_point, end_point)
+extractEdge _ = Nothing
 
 buildDotGraph :: [(String, String)] -> GraphViz.DotGraph String
-buildDotGraph = undefined
+buildDotGraph edgeList = digraph (Str "spanning_tree") $ do
+  forM_ edgeList $ \(start_point, end_point) -> edge start_point end_point []
 
 renderDotGraph :: GraphViz.DotGraph String -> LazyText.Text
-renderDotGraph = GraphViz.renderDot . GraphViz.toDot
+renderDotGraph = renderDot . toDot
