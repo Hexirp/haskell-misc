1280365641cc1546ca1ecb9a691860c80eb9e3c7
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2025-10-06 18:24:05 +0900

    Fix: processEdgeList の実装を修正する
    最短の経路における最後の辺が追加されておらず、異常な出力になっていた
    ため。

--- spanning-tree/src/SpanningTree.hs
+++ spanning-tree/src/SpanningTree.hs
@@ -13,7 +13,7 @@ buildAdjacencyMap = foldr (\(start_node, end_node) -> Map.insertWith (++) start_
 
 bfsShortestPathEdgeListMap :: String -> Map String [String] -> Map.Map String [String]
 bfsShortestPathEdgeListMap rootNode graphMap =
-  bfsShortestPathEdgeListMap_ graphMap (Set.singleton rootNode) (Seq.singleton rootNode) (Map.singleton rootNode [])
+  bfsShortestPathEdgeListMap_ graphMap (Set.singleton rootNode) (Seq.singleton rootNode) (Map.singleton rootNode [rootNode])
 
 bfsShortestPathEdgeListMap_ :: Map.Map String [String] -> Set.Set String -> Seq.Seq String -> Map.Map String [String] -> Map.Map String [String]
 bfsShortestPathEdgeListMap_ graph_map visited_node_set focused_node_queue shortest_path_edge_list_map =
@@ -25,7 +25,7 @@ bfsShortestPathEdgeListMap_ graph_map visited_node_set focused_node_queue shorte
         new_neighbor_node_list = filter (`Set.notMember` visited_node_set) neighbor_node_list
         new_visited_node_set = foldr Set.insert visited_node_set new_neighbor_node_list
         new_focused_node_queue = foldl (|>) rest_focused_node_queue new_neighbor_node_list
-        new_shortest_path_edge_list_map = foldl (\new_map new_node -> Map.insert new_node (current_focused_node : Map.findWithDefault [] current_focused_node new_map) new_map) shortest_path_edge_list_map new_neighbor_node_list
+        new_shortest_path_edge_list_map = foldl (\new_map new_node -> Map.insert new_node (new_node : Map.findWithDefault [] current_focused_node new_map) new_map) shortest_path_edge_list_map new_neighbor_node_list
       in
         bfsShortestPathEdgeListMap_ graph_map new_visited_node_set new_focused_node_queue new_shortest_path_edge_list_map
 
