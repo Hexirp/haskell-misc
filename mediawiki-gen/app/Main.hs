adfeee5a4ecd271e309bf2672679f01ffa0c7340
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2025-10-12 00:17:40 +0900

    Add: mediawiki-gen の実装を追加する

--- mediawiki-gen/app/Main.hs
+++ mediawiki-gen/app/Main.hs
@@ -1,6 +1,28 @@
 module Main (main) where
 
-import Lib
+import System.Environment (getArgs)
+import Text.Read (readMaybe)
+
+permalink :: Word -> String
+permalink oldid =
+  if oldid >= 1
+    then "Special:Permalink/" ++ show oldid
+    else error "Usage: mediawiki-gen-exe <oldid>"
+
+link :: String -> String
+link name = "[[" ++ name ++ "]]"
+
+list :: [String] -> String
+list = concatMap (\text -> "# " ++ text ++ "\n")
+
+permalink100 :: Word -> String
+permalink100 oldid = list $ map (link . permalink) $ take 100 [oldid ..]
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case args of
+    [arg1] -> case readMaybe arg1 of
+      Nothing -> error "Usage: mediawiki-gen-exe <oldid>"
+      Just oldid -> putStrLn $ permalink100 oldid
+    _ -> error "Usage: mediawiki-gen-exe <oldid>"
