78b0498690f033b81c34945f3503d7a6c32fa8ad
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2025-10-12 14:01:34 +0900

    Update: builder を使用する
    通常のリストにおける (++) は左結合だと効率が悪くなるため、それを避ける
    ために、 builder を使用する。

--- mediawiki-gen/app/Main.hs
+++ mediawiki-gen/app/Main.hs
@@ -1,21 +1,25 @@
 module Main (main) where
 
+import Data.Text.IO.Utf8 as Text (putStrLn)
+import Data.Text.Lazy as LazyText (toStrict)
+import Data.Text.Lazy.Builder as LazyTextBuilder (Builder, fromString, toLazyText)
+import Data.Text.Lazy.Builder.Int as LazyTextBuilder (decimal)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 
-permalink :: Word -> String
+permalink :: Word -> LazyTextBuilder.Builder
 permalink oldid =
   if oldid >= 1
-    then "Special:Permalink/" ++ show oldid
+    then LazyTextBuilder.fromString "Special:Permalink/" <> LazyTextBuilder.decimal oldid
     else error "Usage: mediawiki-gen-exe <oldid>"
 
-link :: String -> String
-link name = "[[" ++ name ++ "]]"
+link :: LazyTextBuilder.Builder -> LazyTextBuilder.Builder
+link name = LazyTextBuilder.fromString "[[" <> name <> LazyTextBuilder.fromString "]]"
 
-list :: [String] -> String
-list = concatMap (\text -> "# " ++ text ++ "\n")
+list :: [LazyTextBuilder.Builder] -> LazyTextBuilder.Builder
+list = foldMap (\text -> LazyTextBuilder.fromString "# " <> text <> LazyTextBuilder.fromString "\n")
 
-permalink100 :: Word -> String
+permalink100 :: Word -> LazyTextBuilder.Builder
 permalink100 oldid = list $ map (link . permalink) $ take 100 [oldid ..]
 
 main :: IO ()
@@ -24,5 +28,5 @@ main = do
   case args of
     [arg1] -> case readMaybe arg1 of
       Nothing -> error "Usage: mediawiki-gen-exe <oldid>"
-      Just oldid -> putStrLn $ permalink100 oldid
+      Just oldid -> Text.putStrLn $ LazyText.toStrict $ LazyTextBuilder.toLazyText $ permalink100 oldid
     _ -> error "Usage: mediawiki-gen-exe <oldid>"
