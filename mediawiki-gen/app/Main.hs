b39cfff18f62eccc6f0ef7895adf4858fd82171f
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2025-12-11 20:35:05 +0900

    Update: OverloadedStrings 言語拡張を使用する

--- mediawiki-gen/app/Main.hs
+++ mediawiki-gen/app/Main.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE OverloadedStrings  #-}
+
 module Main (main) where
 
 import Data.Text.IO.Utf8 as Text (putStr)
 import Data.Text.Lazy as LazyText (toStrict)
-import Data.Text.Lazy.Builder as LazyTextBuilder (Builder, fromString, toLazyText)
+import Data.Text.Lazy.Builder as LazyTextBuilder (Builder, toLazyText)
 import Data.Text.Lazy.Builder.Int as LazyTextBuilder (decimal)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
@@ -10,14 +12,14 @@ import Text.Read (readMaybe)
 permalink :: Word -> LazyTextBuilder.Builder
 permalink oldid =
   if oldid >= 1
-    then LazyTextBuilder.fromString "Special:Permalink/" <> LazyTextBuilder.decimal oldid
+    then "Special:Permalink/" <> LazyTextBuilder.decimal oldid
     else error "permalink: non-positive oldid"
 
 link :: LazyTextBuilder.Builder -> LazyTextBuilder.Builder
-link name = LazyTextBuilder.fromString "[[" <> name <> LazyTextBuilder.fromString "]]"
+link name = "[[" <> name <> "]]"
 
 list :: [LazyTextBuilder.Builder] -> LazyTextBuilder.Builder
-list = foldMap (\text -> LazyTextBuilder.fromString "# " <> text <> LazyTextBuilder.fromString "\n")
+list = foldMap (\text -> "# " <> text <> "\n")
 
 permalink1000 :: Word -> LazyTextBuilder.Builder
 permalink1000 oldid = list $ map (link . permalink) $ take 1000 [oldid ..]
