f0700bb2bfc5fffbec681f173ff8b14bb3867c44
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2025-12-11 20:43:18 +0900

    Update: permalink から diff に変更する
    Permalink よりも Diff の方が編集の内容が分かりやすいので、変更する。

--- mediawiki-gen/app/Main.hs
+++ mediawiki-gen/app/Main.hs
@@ -9,11 +9,11 @@ import Data.Text.Lazy.Builder.Int as LazyTextBuilder (decimal)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 
-permalink :: Word -> LazyTextBuilder.Builder
-permalink oldid =
+diff :: Word -> LazyTextBuilder.Builder
+diff oldid =
   if oldid >= 1
-    then "Special:Permalink/" <> LazyTextBuilder.decimal oldid
-    else error "permalink: non-positive oldid"
+    then "Special:Diff/" <> LazyTextBuilder.decimal oldid
+    else error "diff: non-positive oldid"
 
 link :: LazyTextBuilder.Builder -> LazyTextBuilder.Builder
 link name = "[[" <> name <> "]]"
@@ -21,8 +21,8 @@ link name = "[[" <> name <> "]]"
 list :: [LazyTextBuilder.Builder] -> LazyTextBuilder.Builder
 list = foldMap (\text -> "# " <> text <> "\n")
 
-permalink1000 :: Word -> LazyTextBuilder.Builder
-permalink1000 oldid = list $ map (link . permalink) $ take 1000 [oldid ..]
+diff1000 :: Word -> LazyTextBuilder.Builder
+diff1000 oldid = list $ map (link . diff) $ take 1000 [oldid ..]
 
 main :: IO ()
 main = do
@@ -31,6 +31,6 @@ main = do
     [arg1] -> case readMaybe arg1 of
       Nothing -> error "Usage: mediawiki-gen <oldid>"
       Just oldid -> if oldid >= 1
-        then Text.putStr $ LazyText.toStrict $ LazyTextBuilder.toLazyText $ permalink1000 oldid
+        then Text.putStr $ LazyText.toStrict $ LazyTextBuilder.toLazyText $ diff1000 oldid
         else error "Usage: mediawiki-gen <oldid>"
     _ -> error "Usage: mediawiki-gen <oldid>"
